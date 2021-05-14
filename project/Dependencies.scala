7f64b44ca01c12deebb31f5dcc7dfb5cf4a90f58
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-14 20:47:41 +0200

    Update scalatest, scalatest-mustmatchers to 3.2.9 (#73)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val scalatestVersion = "3.2.8"
+  val scalatestVersion = "3.2.9"
   lazy val scalaTest   = Seq(
     "org.scalatest" %% "scalatest"              % scalatestVersion,
     "org.scalatest" %% "scalatest-mustmatchers" % scalatestVersion
